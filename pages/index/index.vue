55c47594d27e7071d1006e1b0fbb48a07250c707
IHuan123 <whuan0322@163.com>
2022-04-11 20:05:29 +0800

    上传整个项目

---
+++ pages/index/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <view>
+    <button @click="show = true">show</button>
+    <w-popup v-model="show" ref="el" z-index="1200" :height="800">
+      <div style="text-align:center;height: 100%;display: flex;align-items: center;justify-content: center;background: red">hello</div>
+    </w-popup>
+  </view>
+</template>
+<script>
+
+export default {
+  data(){
+    return {
+      show:false
+    }
+  },
+}
+</script>
+
+<style scoped lang="scss"></style>
